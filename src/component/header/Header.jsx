211abcc88fddb1e5b2721723c4aa2d77cd0d284c
webdeveloper510 <developerweb6@gmail.com>
2023-04-27 18:30:25 +0530

    add mobile menus icons

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -20,6 +20,17 @@ import '../../assets/css/style.css';
 import '../../assets/css/responsive.css';
 
 import { RxHamburgerMenu } from "react-icons/rx";
+import { SlHome } from "react-icons/sl";
+import { SlWallet } from "react-icons/sl";
+import { IoIosGitCompare } from "react-icons/io";
+import { MdOutlineTransferWithinAStation } from "react-icons/md";
+import { TbNotes } from "react-icons/tb";
+import { MdOutlineRateReview } from "react-icons/md";
+import { TbApps } from "react-icons/tb";
+import { BsQuestionSquare } from "react-icons/bs";
+import { VscCallOutgoing } from "react-icons/vsc";
+import { BiLockOpen } from "react-icons/bi";
+import { CgLogIn } from "react-icons/cg";
 
 
 import logoblue from '../../assets/img/home/logo-blue.png';
@@ -126,34 +137,34 @@ const Header =() => {
         <nav id="navbar" className="navbar">
                   <ul>
                     <li>
-                      <NavLink className=""  to="/" onClick={handleClose}>Home</NavLink>
+                      <NavLink className=""  to="/" onClick={handleClose}><SlHome/>Home</NavLink>
                     </li>
                     <li>
-                      <NavLink to="/Savings" onClick={handleClose}> Savings</NavLink>
+                      <NavLink to="/Savings" onClick={handleClose}><SlWallet/>Savings</NavLink>
                       </li>
                     <li>
-                      <NavLink to="/Compare" onClick={handleClose}> Compare</NavLink>
+                      <NavLink to="/Compare" onClick={handleClose}><IoIosGitCompare/> Compare</NavLink>
                       </li>
                      
 
                       <li>
-                      <NavLink to="/Coverage" onClick={handleClose}> Coverage</NavLink>
+                      <NavLink to="/Coverage" onClick={handleClose}><MdOutlineTransferWithinAStation/>Coverage</NavLink>
                       </li>
                       <li>
-                      <NavLink to="/Plans" onClick={handleClose}> Plans</NavLink>
+                      <NavLink to="/Plans" onClick={handleClose}> <TbNotes/>Plans</NavLink>
                       </li>
                       <li>
-                      <NavLink to="/Blog" onClick={handleClose}> Blog</NavLink>
+                      <NavLink to="/Blog" onClick={handleClose}> <MdOutlineRateReview/>Blog</NavLink>
                       </li>
                       <li>
-                      <NavLink to="/App" onClick={handleClose}> App</NavLink>
+                      <NavLink to="/App" onClick={handleClose}> <TbApps/>App</NavLink>
                       </li>
                   
                     <li>
-                      <NavLink to="/FAQ" onClick={handleClose}>FAQ</NavLink>
+                      <NavLink to="/FAQ" onClick={handleClose}><BsQuestionSquare/>FAQ</NavLink>
                     </li>
                     <li>
-                      <NavLink to="/Contact" onClick={handleClose}>Contact</NavLink>
+                      <NavLink to="/Contact" onClick={handleClose}><VscCallOutgoing/>Contact</NavLink>
                     </li>
 
                         {/* <li class="dropdown">
@@ -168,10 +179,10 @@ const Header =() => {
                         </ul>
                       </li> */}
                         <li>
-                        <NavLink to="/Signup" onClick={handleClose}>Signup</NavLink>
+                        <NavLink to="/Signup" onClick={handleClose}><BiLockOpen/>Signup</NavLink>
                         </li>
                         <li>
-                        <NavLink to="/Login" onClick={handleClose}>Login</NavLink>
+                        <NavLink to="/Login" onClick={handleClose}><CgLogIn/>Login</NavLink>
                         </li>
                        
                  
